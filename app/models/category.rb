a5ae59cddcfd27b3b5d56f3a2c50aac23d16ab16
Raghu Betina <raghu@firstdraft.com>
2018-03-01 22:25:22 +0000

    Direct association between categories and comments

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :category
+
   belongs_to :type
 
   has_many   :flags,
